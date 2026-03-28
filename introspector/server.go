70c195b22657758876ccf84a97ec36fc5c31f9b4
raulk <raul@ethereum.org>
2026-03-28 19:24:42 +0000

    fix: skip gzip for WebSocket upgrade requests

--- introspector/server.go
+++ introspector/server.go
@@ -335,7 +335,8 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 
 func gzipHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") ||
+			strings.EqualFold(r.Header.Get("Upgrade"), "websocket") {
 			next.ServeHTTP(w, r)
 			return
 		}
